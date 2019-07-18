fd3ab25addfaba7f415fdb9ace050a4d8df5b84a
Ray Sargolini <rsargolini@sbcglobal.net>
2019-07-18 09:44:53 -0400

    Final Deployment

--- js/register_scripts.js
+++ js/register_scripts.js
@@ -1,7 +1,5 @@
 // Register Page - Scripts 
 
-// Password - Confirm Password Mismatch
-
 // Don't run any JS until DOM in built
 window.onload = function () {
 
@@ -11,6 +9,7 @@ window.onload = function () {
     // get Alert Message Elements
     var mismatchMsg = document.getElementById('mismatchMsg');
 
+    // Password - Confirm Password Mismatch
     // Bind Submit Event Handler to Form
     registrationForm.onsubmit = function (submitEvent) {
 
@@ -22,19 +21,24 @@ window.onload = function () {
         var inputPasswordValue = inputPassword.value;
         var confirmPasswordValue = confirmPassword.value;
 
-        // Check to ensure User entered values
+        // User did not enter matching Confirm Password
         if (confirmPasswordValue != inputPasswordValue) {
-            // User did not enter matching Confirm Password
             // Show Mismatch Message
             mismatchMsg.style.display = 'block';
+
+            // Reset Password and Confirm Password back to placeholder values
+            inputPassword.value = '';
+            confirmPassword.value = '';
+
+            // Return Cursor focus to Password field
             document.getElementById('inputPassword').focus();
             return false;
         }
     }
+    // On Reset hide Password - Confirm Password error message
     // Bind Submit Event Handler to Form
     registrationForm.onreset = function (resetEvent) {
         mismatchMsg.style.display = 'none';
-        inputFName  ;
     }
 
 }
